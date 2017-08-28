55420c221026d5411d1ba157788d1a04e5f1cc90
theDavidKruse <thedavidkruse@gmail.com>
2017-08-28 14:39:41 -0700

    Remade node server

--- node-coinzy/server.js
+++ node-coinzy/server.js
@@ -6,6 +6,9 @@ var cors = require('cors');
 var logger = require('morgan');
 var knex = require('./db/knex');
 
+var index = require('./routes/indexRoutes');
+var todos = require('./routes/todosRoutes');
+
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -16,31 +19,8 @@ app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-  res.render('index', { title: 'DBConfig' });
-})
-
-app.get('/todos', function(req, res) {
-  res.send('get-all route')
-})
-
-app.get('/todos/:id', function(req, res) {
-  res.send('get-one route')
-})
-
-app.post('/todos', function(req, res) {
-  res.send('add-one route')
-})
-
-app.put('/todos/:id', function(req, res) {
-  res.send('change/update-one route')
-})
-
-app.delete('/todos/:id', function(req, res) {
-  res.send('delete/remove-one route')
-})
-
-
+app.use('/', index);
+app.use('/todos', todos);
 
 app.listen(port, function() {
 console.log("listening on port: ", port);
